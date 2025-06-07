d9541d9046cc95f558632fb5cbb59dcec6543d57
Quang Nguyen <qknguyen95@gmail.com>
2025-06-07 17:17:24 +0100

    feature: enhance scraping functionality with content extraction and cleaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter the URL to scrape:")
@@ -12,6 +12,22 @@ if st.button("Scrape"):
       # But for now, we will just simulate a response
       st.write("Data scraped successfully!")
       result = scrape_website(url)
-      print(result)
+
+      body_content = extract_body_content(result)
+      cleaned_content = clean_body_content(body_content)
+
+      st.session_state.dom_content = cleaned_content
+
+      with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
   else:
       st.error("Please enter a valid URL.")
+
+if "dom_content" in st.session_state:
+   parse_description = st.text_area("Describe what you want to parse?")
+
+   if st.button("Parse Content"):
+      if parse_description:
+        st.write("Parsing the content")
+
+        dom_chunks = split_dom_content(st.session_state.dom_content)
